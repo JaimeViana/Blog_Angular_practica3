4cbcf9bb1dc75a44ac2c6b7d30e0b8991fc034f1
JaimeViana <jaime.viana.jimenez@gmail.com>
2020-08-14 18:42:22 +0200

    instalación de bootstrap, componente blog y filtrado por categoria (faltan estilos)

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service'
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  posts: Post[];
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.postsService.getAllPosts()
+      .then(arrPosts => {
+        this.posts = arrPosts;
+      }).catch(error => console.log(error));
   }
 
+  async onChange($event) {
+    // console.log($event.target.value);
+    if ($event.target.value === 'todas') {
+      this.posts = await this.postsService.getAllPosts();
+    } else {
+      this.posts = await this.postsService.getPostsByCategoria($event.target.value);
+    }
+  }
 }
